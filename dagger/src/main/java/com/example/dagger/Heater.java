d289b5e69171ac73cdfd92624c4b85dbaad3d3d8
qinhulin <qinhulin@gmail.com>
2025-05-08 15:22:16 +0800

    删除dagger使用示例

--- dagger/src/main/java/com/example/dagger/Heater.java
+++
@@ -1,9 +0,0 @@
-package com.example.dagger;
-
-/** 加热器
- * @author qinhu
- * @date 2025/4/24 18:35
- */
-public interface Heater {
-    void heater();
-}
--- dagger/src/main/java/com/example/dagger/Pump.java
+++
@@ -1,9 +0,0 @@
-package com.example.dagger;
-
-/** 离心泵
- * @author qinhu
- * @date 2025/4/24 18:36
- */
-public interface Pump {
-    void pump();
-}
